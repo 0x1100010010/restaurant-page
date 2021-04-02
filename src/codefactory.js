b810aa04b3b41bc5c493515c350e6763fb038d85
bashforger <bash.forger@gmail.com>
2021-04-02 20:50:09 +0500

    Refactored code

--- src/codefactory.js
+++ src/codefactory.js
@@ -1,8 +1,6 @@
-export default function codefactory( _id, _type, _class, _content, _parent_id ) {
+export default function codefactory( _type, _attr, _content, _parent_id ) {
   let node = document.createElement(_type);
-  node.className = _class
   node.innerText = _content
-  node.setAttribute('id', _id)
+  for (let attr in _attr) { node.setAttribute(attr, _attr[attr]) }
   document.getElementById(_parent_id).appendChild(node)
-  console.log(_content)
 }
--- src/header.js
+++ src/header.js
@@ -1,10 +1,10 @@
 import codefactory from './codefactory';
 
 export default function header (){
-  // codefactory( _id, _type, _class, _content, _parent_id )
-  codefactory('header', 'header', 'text-gray-600 body-font', '', 'content');
-  codefactory('headercontainer', 'div', 'container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center', '', 'header');
-  codefactory( '', 'a', 'title-font cursor-pointer font-medium items-center text-gray-900', 'Le-Hotel', 'headercontainer' );
-  codefactory('headerlinks', 'nav', 'md:ml-auto flex flex-wrap items-center text-base justify-center', '', 'headercontainer');
-  ['tab1', 'tab2', 'tab3'].forEach ( el => codefactory('', 'a', 'mr-5 hover:text-gray-900 cursor-pointer', el, 'headercontainer')  );
+  // codefactory( _type, {_attr}, _content, _parent_id ) 
+  codefactory('header', {'class': 'text-gray-600 body-font', 'id': 'header' }, '', 'content');
+  codefactory('div', {'class':'container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center', 'id': 'headercontainer'}, '', 'header');
+  codefactory( 'a', {'class': 'title-font cursor-pointer font-medium items-center text-gray-900', 'href': '#'}, 'Le-Hotel', 'headercontainer' );
+  codefactory('nav', {'class': 'md:ml-auto flex flex-wrap items-center text-base justify-center', 'id': 'headerlinks'}, '', 'headercontainer');
+  ['Home', 'Menu', 'Contact'].forEach ( el => codefactory('a', {'class': 'mr-5 hover:text-gray-900 cursor-pointer', 'id': el }, el, 'headercontainer') );
 }
